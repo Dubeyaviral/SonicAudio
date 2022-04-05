8f3ab948b1038d5aeefea30e33bd84b9891b886c
Aviral <aviraldubey2000@gmail.com>
2022-04-05 21:14:18 +0530

    adding login, register,products page

--- src/app.js
+++ src/app.js
@@ -6,11 +6,32 @@ const app = express();
 
 const staticPath = path.join(__dirname, "../public");
 
+
 app.use(express.static(staticPath));
 
 
 app.get('/',(req,res)=>{
     res.render('index');
+});
+
+app.get('/about',(req,res)=>{
+    res.sendfile('public/about.html');
+});
+
+app.get('/contact',(req,res)=>{
+    res.sendfile('public/contact.html');
+});
+
+app.get('/login',(req,res)=>{
+    res.sendfile('public/login.html');
+});
+
+app.get('/register',(req,res)=>{
+    res.sendfile('public/register.html');
+});
+
+app.get('/product',(req,res)=>{
+    res.sendfile('public/product.html');
 });
 
 app.listen(port,()=>{
