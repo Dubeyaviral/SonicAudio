77d7d477181268b8e75f04d8a9a0d4499da12431
Aviral <aviraldubey2000@gmail.com>
2023-05-21 17:34:59 +0530

    new

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect("mongodb://localhost:27017/HeadphoneOrder", )
+// mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{
     console.log('connection successful')
